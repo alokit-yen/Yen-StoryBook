db307db373f61e78bb40ddd985580b0515542f70
alokit-yen <alokit.ranjan@yendigital.com>
2025-05-30 18:04:45 +0530

    Image Added

--- src/ALOKIT/Card.stories.jsx
+++ src/ALOKIT/Card.stories.jsx
@@ -1,11 +1,17 @@
 // Card.stories.jsx
 import React from 'react';
-import Card from './Card'; 
+import Card from './Card';
 
 export default {
   title: 'ALOKIT/Card',
-  component: Card, 
-  tags: ['autodocs'],  
+  component: Card,
+  tags: ['autodocs'],
+  argTypes: {
+    imageUrl: {
+      control: 'text',
+      description: 'Optional image URL to display at the top of the card',
+    },
+  },
 };
 
 const Template = (args) => <Card {...args} />;
@@ -15,6 +21,14 @@ Default.args = {
   title: 'Card Title',
   content: 'This is the main content of the card.',
   footer: 'Footer text',
+};
+
+export const WithImage = Template.bind({});
+WithImage.args = {
+  title: 'Card with Image',
+  content: 'This card includes an image at the top.',
+  footer: 'Footer text',
+  imageUrl: 'https://picsum.photos/400/200',
 };
 
 export const NoFooter = Template.bind({});
